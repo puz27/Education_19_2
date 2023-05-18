b12be06848139951196c608f7f06d734fb861809
anv <anv@woori.ru>
2023-05-18 15:01:25 +0300

    add feedback

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from catalog.views import index
+from catalog.views import index, contacts
 
 urlpatterns = [
-    path("", index)
+    path("", index, name="main"),
+    path("contacts", contacts, name="contacts")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "catalog/index.html")
+    content = {"Title": "Main Page"}
+    return render(request, "catalog/index.html", content)
+
+
+def contacts(request):
+    name = request.POST.get("name")
+    surname = request.POST.get("surname")
+    email = request.POST.get("email")
+    feedback = request.POST.get("feedback")
+    content = {"Title": "Contacts Page"}
+
+    print(name, surname, email, feedback)
+    return render(request, "catalog/contacts.html", content)
