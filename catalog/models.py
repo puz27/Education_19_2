3b406bb1cfd358980af71e52f29d79649d22f1df
anv <anv@woori.ru>
2023-05-22 17:33:42 +0300

    4 DONE

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from config import settings
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="product_name")
     description = models.TextField(null=True, blank=True, verbose_name="product_description")
-    image = models.ImageField(upload_to="/media/")
+    image = models.ImageField(upload_to="media")
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name="product_price")
     time_create = models.DateField(auto_now_add=True, verbose_name="product_create")
@@ -14,3 +16,5 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="category_name")
     description = models.TextField(null=True, blank=True, verbose_name="category_description")
+    time_create = models.DateField(auto_now_add=True)
+
